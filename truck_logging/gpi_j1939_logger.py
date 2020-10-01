3da1ad7037f52ec7209f7c9f9a7d60d205910dda
Matthias Melitzki <m.melitzki@gpi-stenn.de>
2020-10-01 18:06:25 +0200

    update system can init logging

--- truck_logging/gpi_j1939_logger.py
+++ truck_logging/gpi_j1939_logger.py
@@ -9,26 +9,71 @@ import json
 
 import can
 import j1939
+import os,time
+
 
 #import xj1939 as j1939
 import logging
 import pretty_j1939.parse
+import subprocess
 
-pretty_j1939.parse.init_j1939db()
+def run_linuxprocess(cmd):
+    process = subprocess.Popen(cmd.split(),
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.STDOUT)
+    returncode = process.wait(timeout=2)
+    logging.debug(f"{cmd} return: {returncode}")
+    retdata=process.stdout.read().decode()
+    logging.debug(retdata)
 
-describer = pretty_j1939.parse.get_describer(describe_pgns=True, describe_spns=True,
-                                             describe_link_layer=True, describe_transport_layer=True,
-                                             include_transport_rawdata=True,
-                                             include_na=False)
+def init_can_system(candev:str):
 
-lLevel = logging.WARN
-logger = logging.getLogger()
+    '''
+
+    :param candev:
+    :return:
+    '''
+    try:
+        run_linuxprocess(f'whoami')
+        #run_linuxprocess(f'ip -a')
+        run_linuxprocess(f'sudo /sbin/ip link set {candev} down')
+        run_linuxprocess(f'sudo /sbin/ip link set {candev} up type can bitrate 500000 restart-ms 1000')
+        run_linuxprocess(f'sudo /sbin/ip link set {candev} txqueuelen 65536')
+    except subprocess.CalledProcessError as e:
+        logging.debug(e)
+
+    #sudo ip link set can1 txqueuelen 65536
+#sudo ip link set can0 up type can bitrate 500000
+
+def init_prettyj1939(pgns=True,spns=True):
+    pretty_j1939.parse.init_j1939db()
+
+
+    describer = pretty_j1939.parse.get_describer(describe_pgns=pgns, describe_spns=spns,
+                                                 describe_link_layer=True,
+                                                 describe_transport_layer=False,
+                                                 include_transport_rawdata=False,
+                                                 include_na=False)
+    return describer
+
+
+
 
 #65282,65280, 65281,65296,65226,65284, Exhaust Emission Controller( 61)
-if 1:
-    logger.setLevel(lLevel)
+def setlogging(level_name):
+    logger = logging.getLogger()
+    loglevel = logging.DEBUG
+    try:
+        loglevel=getattr(logging, level_name.upper())
+        logger.setLevel(loglevel) # type: ignore
+    except AttributeError:
+        logger.setLevel(loglevel)
+
+    logger.debug("Logging set to {}".format(logging.getLevelName(loglevel)))
+
+    #logging.basicConfig( level= logging.getLevelName(levelstr))#filename=logfile,
     ch = logging.StreamHandler()
-    ch.setLevel(lLevel)
+    ch.setLevel(loglevel)
     chformatter = logging.Formatter('%(name)25s | %(threadName)10s | %(levelname)5s | %(message)s')
     ch.setFormatter(chformatter)
     logger.addHandler(ch)
@@ -53,7 +98,7 @@ def parse_arguments():
                         help=textwrap.dedent('''\
     command line verbosity
     How much information do you want to see at the command line?
-    You can add several of these e.g., -vv is DEBUG'''), default=2)
+    You can add several of these e.g., -vv is DEBUG'''), default=1)
 
     parser.add_argument('-x', '--hex-out',
                         action='store_true',
@@ -93,10 +138,12 @@ def parse_arguments():
         "source": 0
       }
     ]
+    
+    
 
     '''))
 
-    parser.add_argument('-c', '--channel',
+    parser.add_argument('-c', '--channel',default='can0',
                         help=textwrap.dedent('''\
     Most backend interfaces require some sort of channel.
     For example with the serial interface the channel might be a rfcomm device: "/dev/rfcomm0"
@@ -126,6 +173,8 @@ if __name__ == "__main__":
     verbosity = args.verbosity
     logging_level_name = ['critical', 'error', 'warning', 'info', 'debug', 'subdebug'][min(5, verbosity)]
     can.set_logging_level(logging_level_name)
+    setlogging(logging_level_name)
+    logging.info("Start")
 
     filters = []
     if args.pgn is not None:
@@ -149,29 +198,35 @@ if __name__ == "__main__":
     print("filter source : ", args.source)
     print("filters       : ", filters)
 
-    bus = j1939.Bus(channel=args.channel, bustype=args.interface, j1939_filters=filters, timeout=0.1)
-    print("channel info  : ", bus.can_bus.channel_info)
-    log_start_time = datetime.datetime.now()
-    print('can.j1939 logger started on {}\n'.format(log_start_time))
-    logger.info('can.j1939 logger started on {}\n'.format(log_start_time))
+    if args.channel != 'vcan0':
+        init_can_system(args.channel)
 
+    bus = j1939.Bus(channel=args.channel, bustype=args.interface, j1939_filters=filters, timeout=0.1)
+    logging.info(f"channel info  : {bus.can_bus.channel_info} ")
+    log_start_time = datetime.datetime.now()
+    logging.info(f'can.j1939 logger started on {log_start_time}')
+
+    describer=init_prettyj1939(pgns=True,spns=False)
+    notifier = can.Notifier(bus, [ can.Printer()])#can.Logger("logfile.asc")
     try:
-        for msg in bus:
-            if args.hex_out:
-                msg.display_radix = 'hex'
-            else:
-                msg.display_radix = 10
-            description = describer(msg.data, msg.arbitration_id.can_id)
-            print (description)
-            if bool(description) is True:
-                #print(description)
-                pedal=description.get("Accelerator Pedal Position 1")
-                if pedal:
-                    pass
-                    #print (msg.timestamp,pedal)
-            #print (msg.timestamp,msg.arbitration_id.priority,msg.source,msg.arbitration_id.destination_address_value,msg.pgn,msg.data,)
-            #print(msg)
-            #logger.info(msg)
+        while True:
+        #for msg in bus:
+        #    if args.hex_out:
+        #        msg.display_radix = 'hex'
+        #    else:
+        #        msg.display_radix = 10
+        #    description = describer(msg.data, msg.arbitration_id.can_id)
+        #    print(description)
+        #    if bool(description) is True:
+        #        # print(description)
+        #        pedal = description.get("Accelerator Pedal Position 1")
+        #        if pedal:
+        #            pass
+                    # print (msg.timestamp,pedal)
+            # print (msg.timestamp,msg.arbitration_id.priority,msg.source,msg.arbitration_id.destination_address_value,msg.pgn,msg.data,)
+            # print(msg)
+            # logger.info(msg)
+            time.sleep(0.2)
     except KeyboardInterrupt:
         bus.shutdown()
         print()
